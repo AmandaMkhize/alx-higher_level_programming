430059b7960fc00b7f9e2c7ffff9b1c3aec563d3
Amanda Mkhize <amandamkhize@yahoo.com>
2024-02-06 03:44:37 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function for reading text files."""
+"""Defines a text file-reading function."""
 
 
-def read_text_file(filename=""):
-    """Prints the contents of a UTF-8 encoded text file to the standard output."""
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
