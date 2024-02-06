44cdf922bcf7018fc2a50a994e58ea1b3c5d8b43
Amanda Mkhize <amandamkhize@yahoo.com>
2024-02-06 09:51:30 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    obj = json.loads(my_str)
+    return obj
