79cb3f6e5d6b20745f91446a90bf1e71c4a5a011
Amanda Mkhize <amandamkhize@yahoo.com>
2024-04-27 06:21:00 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-<<comment
-Script that shows the Content-Length from a HTTP request
-comment
+# Script that shows the Content-Length from a HTTP request
 curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
