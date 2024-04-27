c8b83c48c8194aca5b798d31fd225ab9ac82e35b
Amanda Mkhize <amandamkhize@yahoo.com>
2024-04-27 07:23:10 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that makes a request to trigger a specific response
-# Usage: ./trigger_response.sh
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" >/dev/null 2>&1 &
+# Sends a request to trigger a specific response
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" >/dev/null 2>&1
